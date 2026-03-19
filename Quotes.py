685e0c9e5661da93c0ce65b6d0d874b06af62d76
kabsito <msm2009games@gmail.com>
2026-03-19 08:29:38 +0100

    Primer subida de archivos

---
+++ Quotes.py
@@ -0,0 +1,9 @@
+word = "planet"
+print (word)
+
+sentence = "Planet Earth is in the solar system"
+print (sentence)
+
+paragraph = """Planet Earth is in the solar system and holds life
+ of millions of species arroud itself"""
+print (paragraph)
