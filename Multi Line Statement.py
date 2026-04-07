42ffade6fa3d03d34fc2ae5719f45a12d21d5472
kabsito <msm2009games@gmail.com>
2026-04-07 12:20:06 +0200

    multi line statements and if and else resources

---
+++ Multi Line Statement.py
@@ -0,0 +1,6 @@
+total = 34 + \
+        2 + \
+        8
+print (total)
+days =["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+print (days)
---
+++ Unit.1/If and Else.py
@@ -0,0 +1,8 @@
+if True:
+    print ("Answer")
+    print ("True")
+else:
+    print ("Answer")
+    print ("False")
+
+
