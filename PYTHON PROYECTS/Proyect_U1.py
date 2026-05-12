22a298f43caf4d15733ed4f2ef99385f883273bc
kabsito <msm2009games@gmail.com>
2026-05-12 10:10:29 +0200

    Proyect U1

---
+++ PYTHON PROYECTS/Proyect_U1.py
@@ -0,0 +1,12 @@
+day = "Monday"
+number = "11th"
+month = "August"
+print ("Hello and welcome to my first proyect")
+print ("This is a pryect for you to have fun")  
+
+x = 10
+while x >=1:
+    x =x-1
+    print (x)
+print (f"Today is {day} {number} of {month}!")
+
---
+++ PYTHON UNIT 1/Exercise7_f-strings.py
@@ -0,0 +1,4 @@
+cats = 12
+dogs = 3
+
+print (f"I have {cats} cats and {dogs} dogs")
