c48f522d5fcb996c3a25564cc613f371d2cd46a0
kabsito <msm2009games@gmail.com>
2026-06-10 11:56:46 +0200

    Upper and lower

---
+++ PYTHON UNIT 2/Exercise7_Upper and lower.py
@@ -0,0 +1,9 @@
+phrase = "Hello there"
+
+print (phrase.capitalize())
+
+phrase2 = "General Kenobi..."
+print (phrase2.lower())
+phrase3 = "you are brave coming here, kill him."
+print (phrase3.upper())
+
