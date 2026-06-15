2d0bb7992f61b750642e4c6e7bf7bb945d8a9264
kabsito <msm2009games@gmail.com>
2026-06-15 13:19:36 +0200

    And

---
+++ PYTHON UNIT 2/Exercise8_And.py
@@ -0,0 +1,7 @@
+n1 = 5 
+n2 = 24
+n3 = 7
+
+print ("hello and welcome today we will compare a few numbers and make some operations")
+
+print (f"El número n1 es menos que n2 y n3? \n {n1<n2 and n1<n3}")#\n is used to enter a new line it uses as the enter button.
