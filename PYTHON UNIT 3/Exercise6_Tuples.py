2afbcaf178b609fd040a5e943762e2c3265917bd
Kabsito <msm2009games@gmail.com>
2026-07-28 01:43:54 +0200

    tuples

---
+++ PYTHON UNIT 3/Exercise6_Tuples.py
@@ -0,0 +1,14 @@
+tuples = (1, 2, 3, 4, 5)
+print (tuples)
+tuple2 = 1,2,3,4,5,55
+print (type(tuple2))
+print (tuple2)
+
+#tuple2 [0] = 5 This makes an error because tuples are unmutable!
+
+tuple2 = 1,2,3,4,5,55
+print (tuple2[3]) #You still can view the number or variable of the tuples in one position
+
+list1 = [1,2,3,4,5]
+tuplelist = tuple(list1)
+print (type(tuplelist))
