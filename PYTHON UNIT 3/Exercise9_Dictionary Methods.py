58b52bc4bc06aeba654503c4b2499d352209fc49
Kabsito <msm2009games@gmail.com>
2026-08-01 15:28:49 +0200

    methods

--- PYTHON UNIT 3/Exercise9_Dictionary Methods.py
+++ PYTHON UNIT 3/Exercise9_Dictionary Methods.py
@@ -6,5 +6,13 @@ dict= {"a": 3, "b": 5}
 it = (dict.items())
 print (it)
 
-k = (dict.key())
+k = (dict.keys())
 print (k)
+
+v = (dict.values())
+print (v)
+
+result = (dict.fromkeys(dict, 0))
+print (result)
+
+updte = (dict.update({"a": 3, ""}))
