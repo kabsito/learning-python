3f72779911b5aa999846db4f048f2bef865e9d4b
Kabsito <msm2009games@gmail.com>
2026-08-03 15:36:19 +0200

    set and removals

--- PYTHON UNIT 3/Exercise10_Remove and set().py
+++ PYTHON UNIT 3/Exercise10_Remove and set().py
@@ -5,3 +5,12 @@ print (list1)
 list2 = {"potatoe", "apple", "tomatoe"}
 list2.discard("potatoe")
 print (list2)
+
+list3 =["Atlanta", "New York", "Madrid", "Barcelona"]
+print ("The removed city is", list3.pop())#The last one is selected
+print ("The city in the second place is", list3.pop(1))
+list3sub1 = (list3.clear())
+print (list3sub1)
+
+s = set([7,4,8,1,9,3,2,6,1])
+print (s)
