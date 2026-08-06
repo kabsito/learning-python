9371b146e0a2af1a5968b814530e4b59c22e3265
Kabsito <msm2009games@gmail.com>
2026-08-06 16:47:07 +0200

    proyect mid unit3

--- PYTHON PROYECTS/Proyect_U3.py
+++ PYTHON PROYECTS/Proyect_U3.py
@@ -7,10 +7,13 @@ movie3 = input("Now enter the name of the third movie please ")
 movie4 = input("Now enter the name of the fourth movie please ")
 movie5 = input("Now enter the name of the fifth movie please ")
 
-movielist = (movie1, movie2, movie3, movie4, movie5)
+movielist = [movie1, movie2, movie3, movie4, movie5]
 print (f"This is your movie list:{movielist}")
 
-n1 = input('''Now you have 3 options to choose between:
- - First, putting 1 in the console will let you add 2 more movies to the list
- - Second
- - Third''')
+n1 = input('''Now if you wan to add 2 more films put number 1, if not put any other number please.''')
+if n1 == "1":
+    movie6 = input("Enter you next movie ")
+    movie7 = input("Enter your final movie ") 
+    movielist = [movie1, movie2, movie3, movie4, movie5, movie6, movie7]
+
+print(f"Your actual movie list is this:{movielist}")
