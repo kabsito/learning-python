b90c7c0502f1a647a0163427575da4cde6c0711c
Kabsito <msm2009games@gmail.com>
2026-08-11 16:13:27 +0200

    len ()

---
+++ PYTHON UNIT 3/Exercise12_len().py
@@ -0,0 +1,8 @@
+message = input("Enter a message to ensure it has more than 3 letters ")
+
+length = (len(message))
+
+if length < 4:
+    print ("YOUR NUMBER HAS 3 O LESS LETTERS")
+else:
+    print("Your message has more than 3 letters")
