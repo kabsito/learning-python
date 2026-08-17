598ce338ca9978aeddd0aa3eff5c56753abe295e
Kabsito <msm2009games@gmail.com>
2026-08-17 23:02:49 +0200

    abs()

---
+++ PYTHON UNIT 3/Exercise15_Abs().py
@@ -0,0 +1,9 @@
+print (abs(-123)) #Must be an integer, float, or complex number
+
+print (abs(-1.876))
+
+print (abs (2))
+
+print (abs(3 + 4j))#It also calculates complex numbers by doing the root of the sum of both numbers ^2
+
+print (abs(x - y))#This is an error because is not a int, float, or complex number
