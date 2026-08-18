2085eb00f13c875d4743fb373c205ad8978d067f
Kabsito <msm2009games@gmail.com>
2026-08-18 19:37:18 +0200

    sorted()

---
+++ PYTHON UNIT 3/Exercise17_sorted().py
@@ -0,0 +1,17 @@
+number = [1,8906,5,8,7,34,2,14]
+
+print (number)
+
+print (sorted(number))
+
+print (sorted(number, reverse=True))
+
+print (sorted("Pizza Hut"))
+
+list1 = ("cheddar", "olive", "apple", "meatballs")
+
+print (list1)
+
+print (sorted(list1))
+
+print (sorted(list1, key=len))
