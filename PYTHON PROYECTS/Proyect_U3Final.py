6ff5f58b00397328ac771181acf98895a5493339
Kabsito <msm2009games@gmail.com>
2026-08-21 16:51:31 +0200

    Proyect U3 final 1nd part

--- PYTHON PROYECTS/Proyect_U3Final.py
+++ PYTHON PROYECTS/Proyect_U3Final.py
@@ -8,12 +8,12 @@ Second""")
 
 print ("""It will be very easy, I will show you a list of words and you will have to type what is the incorrect word, it will add +1 to a score number you will be able to see at the endo of the hole game""")
 
-
-question1 = ("tomatoe", "lettuce", "fish", "potatoe")
+#-----------------------------------------------------------------------------------
+question1 = ["tomatoe", "lettuce", "fish", "potatoe"]
 print (question1)
 
 
-solution = ("fish")
+solution = question1.pop(2)
 
 
 answer1 = (input("Now write exactly the same word you think is incorrect "))
@@ -21,12 +21,13 @@ if answer1 == solution:
     score = (score + 1)
     print ("Your answer was correct, you've earned 1 point with this question")
 else:
-    print ("Your answer was incorrect, you'll not earn points")
+    print ("Your answer was incorrect, you'll not earn points, the correct answer was")
+    print (solution)
 
-
-question2 = ("Mercedes", "Mclaren", "Williams", "Cadilac")
+#------------------------------------------------------------------------------------
+question2 = ["Mercedes", "Mclaren", "Williams", "Cadilac"]
 print (question2)
-solution2 = ("Cadilac")
+solution2 = question2.pop(3)
 
 
 answer2 = (input("Now write exactly the same word you think is incorrect  "))
@@ -36,12 +37,13 @@ if answer2 == solution2:
     score = (score + 1)
     print ("Your answer was correct, you've earned 1 point with this question")
 else:
-    print ("Your answer was incorrect, you'll not earn points")
+    print ("Your answer was incorrect, you'll not earn points, the correct answer was")
+    print (solution2)
 
-
-question3 = ("scar", "m16", "pump", "barret")
+#------------------------------------------------------------------------------------
+question3 = ["scar", "m16", "pump", "barret"]
 print (question3)
-solution3 = ("m16")
+solution3 = question3.pop(1)
 
 
 answer3 = (input("Now write exactly the same word you think is incorrect "))
@@ -51,9 +53,10 @@ if answer3 == solution3:
     score = (score + 1)
     print ("Your answer was correct, you've earned 1 point with this question")
 else:
-    print ("Your answer was incorrect, you'll not earn points")
+    print ("Your answer was incorrect, you'll not earn points, the correct answer was")
+    print (solution3)
 
-
+#-----------------------------------------------------------------------------------
 print ("Now we'll move to the second game")
 print ("Here you have to tell me the correct number rounded up and the absolute of them")
 
@@ -62,7 +65,7 @@ print ("Here you have to tell me the correct number rounded up and the absolute 
 
 number1 = 6345.448  
 print (F"This is the first number, for the answer just type the number please {number1}")
-answernumber1 = (input("Enter the number here "))
+answernumber1 = int(input("Enter the number here "))
 
 
 solutionnumber1 = (round(number1))
@@ -74,25 +77,17 @@ if solutionnumber1 == answernumber1:
 else:
     print ("Your answer was incorrect, you won't earn points for this question")
 
+numberx = -234.9
 
-number2 = -575.7  
-print (F"This is the first number, for the answer just type the number please {number2}")
-answernumber2 = (input("Enter the number here "))
+absolute = (round(numberx))
 
+solution4 = (abs(absolute))
 
-solutionnumber2 = (abs(round(number2)))
+print (F"This is the second number, for the answer just type the number please {numberx}")
+answernumber2 = int(input("Enter the number here "))
 
-
-if solutionnumber2 == answernumber2:
-    print ("Your answe was correct you've won 1 point more")
+if answernumber2 == solution4:
+    print ("Your answer was correct")
     score = (score + 1)
 else:
-    print ("Your answer was incorrect, you won't earn points for this question")
-
-
-
-
-print ("Now for the third and last question you'll have to")
-
-
-
+    print ("Your answer was incorrect")
