6c7302586dcebc374ef0bab59d3a4dc843f9f0b4
Kabsito <msm2009games@gmail.com>
2026-08-26 20:37:53 +0200

    infinite controlled

---
+++ PYTHON UNIT 4/Exercise2_Infinite controled.py
@@ -0,0 +1,9 @@
+while True:
+    text = input ("Press the leter k to end: ")
+    if text == "k":
+     print ("You exited the program" )
+     break
+    
+    print (f"You said {text}")
+    
+   
