3d18668a6d2638d4e8196af156d91f93bea84bb1
Kabsito <msm2009games@gmail.com>
2026-09-02 22:15:44 +0200

    range ()

---
+++ PYTHON UNIT 4/Exercise4_Range().py
@@ -0,0 +1,17 @@
+x = 0
+
+for x in range(8):
+    print (x)
+    x = x+1
+
+for x in range (5,12):
+    print (x)
+
+y = 0
+
+for y in range (0,10,2):#Esto hace que salte de 2 en 2
+    print (y)
+
+
+for i in range (9,0,-1):
+    print (i)
