0b4de5cbbc5471c0fe3c9141d14275c263421dbc
Kabsito <msm2009games@gmail.com>
2026-09-11 00:10:24 +0200

    list comprehension

--- PYTHON UNIT 4/Exercise40_List Comprehension.py
+++ PYTHON UNIT 4/Exercise40_List Comprehension.py
@@ -1,5 +1,12 @@
 squareroot = []
 for i in range (5):
     squareroot.append(i**2) #is the same as the next thing but shorter
+print (squareroot)
 
 squareroot = [i**2 for i in range (5)]
+print (squareroot)
+
+list = [10, 20, 30, 40]
+
+newlist = [i/10 for i in list ]
+print (newlist)
