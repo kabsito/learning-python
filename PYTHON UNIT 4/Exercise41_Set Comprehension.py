52656fe085f8182ac498d47a40e01622d2ec75df
Kabsito <msm2009games@gmail.com>
2026-09-12 20:06:36 +0200

    set comprehension

---
+++ PYTHON UNIT 4/Exercise41_Set Comprehension.py
@@ -0,0 +1,6 @@
+#Is th same as the last one bujt it doesn't allow duplicates
+
+phrase = ("I like playing basketball with my friends")
+phraseset = {i for i in phrase if i =='i'}
+
+print (phraseset)
