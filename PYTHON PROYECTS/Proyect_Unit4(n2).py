bc896eb65e7198e07bab10d67232607b2629191a
Kabsito <msm2009games@gmail.com>
2026-09-20 21:45:16 +0200

    Unit 4 proyect2 started

---
+++ PYTHON PROYECTS/Proyect_Unit4(n2).py
@@ -0,0 +1,13 @@
+print ("Welcome to your shoping list!")
+
+options =(input('''What would you like to do?
+1 - Create a new shopping list
+2 - See your last shopping list
+3 - exit'''))
+option = int(input("Press the button that corresponds to the action you want to do"))
+while option >3 :
+    while option<0:
+        option =int(input("Press the button that corresponds to the action you want to do"))
+
+print (option)
+
