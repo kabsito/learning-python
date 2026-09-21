6fa83bd9185cde772e64b6e95ab4abc047d26416
Kabsito <msm2009games@gmail.com>
2026-09-21 22:55:00 +0200

    debug

--- PYTHON PROYECTS/Proyect_Unit4(n2).py
+++ PYTHON PROYECTS/Proyect_Unit4(n2).py
@@ -5,7 +5,12 @@ option =int(input('''What would you like to do?
 2 - See your last shopping list
 3 - exit'''))
 option = int(input("Press the button that corresponds to the action you want to do "))
-while option >0:
-    while option <3:
-            option =int(input("Press the button that corresponds to the action you want to do " ))
-print (0)
+while option >3:
+     while option <1:
+        option =int(input("Press the button that corresponds to the action you want to do"))
+        if option ==1:
+            break
+        if option ==2:
+            break
+        if option ==3:
+            break
