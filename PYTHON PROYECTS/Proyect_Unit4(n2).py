a0264b357bb8e57d03f7945ba63c470189f3df11
kabsito <msm2009games@gmail.com>
2026-09-23 09:54:59 +0200

    proyect unit 4 (2)

--- PYTHON PROYECTS/Proyect_Unit4(n2).py
+++ PYTHON PROYECTS/Proyect_Unit4(n2).py
@@ -1,12 +1,17 @@
 print ("Welcome to your shoping list!")
 
-option =int(input('''What would you like to do?
+print('''What would you like to do?
 1 - Create a new shopping list
 2 - See your last shopping list
-3 - exit'''))
+3 - exit''')
+option = int(input("Now enter the number that corresponds to the action you want to make "))
 while True:
-     option =int(input("Press the button that corresponds to the action you want to do "))
      if option in [1,2,3]:
           break
      else:
+          option =int(input("Press the button that corresponds to the action you want to do "))
           continue
+
+if option == 1:
+     print ('''Some things for the creation of your list, it can contain up to 20 elements and in the case" \
+     " you want to finish you should put 0 instead''')
