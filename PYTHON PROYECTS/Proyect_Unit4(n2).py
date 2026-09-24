a77e450e6fdd51f39625fae8529e275d769e9c21
kabsito <msm2009games@gmail.com>
2026-09-24 12:47:50 +0200

    unit 4 proyect 2

--- PYTHON PROYECTS/Proyect_Unit4(n2).py
+++ PYTHON PROYECTS/Proyect_Unit4(n2).py
@@ -13,5 +13,24 @@ while True:
           continue
 
 if option == 1:
-     print ('''Some things for the creation of your list, it can contain up to 20 elements and in the case" \
-     " you want to finish you should put 0 instead''')
+     print ('''Some things for the creation of your list, it can contain up to 20 elements and in the case \nyou want to finish you should put 0 instead''')
+
+list = []
+cant1 = 20
+element = 1
+while cant1 != 0:
+     cant = (cant1 -1)
+     item = (input(f"Enter the element number {element} of the list \n"))
+     if item == "0":
+          cant1= 0
+          break
+     element = element + 1 
+     list.append(item)
+     print (f"Your actual shopping list is this {list}")
+
+print ("Now when you have taken a item of your shopping list you can mark it and it will helo you to keep track \nof the items you alrready have!")
+print ("You must write the same item and it will apear with a ✔️\n")
+
+variable1 = (input('''Now you can do several things:
+'''))
+call1 = (input("Enter the name of the item you want to check "))
